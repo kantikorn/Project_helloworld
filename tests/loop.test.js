b2a55069b6ede032067c353a4f248ce9e8501485
Kantikorn <63209010005@sbtc.ac.th>
2024-05-14 14:13:12 +0700

    Commit Fuction false data for Loop!

--- tests/loop.test.js
+++ tests/loop.test.js
@@ -3,6 +3,6 @@ const loopNumbers = require('../loop');
 test('loop from 1 to 10', () =>{
     const numbers = loopNumbers();
     for (const num of numbers) {
-        expect(num).toBeLessThanOrEqual(10); // ตรวจสอบว่าตัวเลขทั้งหมดไม่เกิน 10
+        expect(num).toBeLessThanOrEqual(15); // ตรวจสอบว่าตัวเลขทั้งหมดไม่เกิน 10
     }
 });
