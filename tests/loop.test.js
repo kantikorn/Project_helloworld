438ec20abd38b1f2a3ecdbff1049577f0e3a931f
Kantikorn <63209010005@sbtc.ac.th>
2024-05-15 07:30:24 +0700

    Update loop.test.js

--- tests/loop.test.js
+++ tests/loop.test.js
@@ -1,5 +1,5 @@
 const loopNumbers = require('../loop');
 
 test('loop from 1 to 10', () =>{
-    expect(loopNumbers()).toEqual([1,2,3,4,5,6,7,8,9,10]);
+    expect(loopNumbers()).toEqual([1,2,3,4,5,6,7,8,9,11]);
 })
