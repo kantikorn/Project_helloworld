095381bd93f6f0d49261d1fcf4910223bfb51c3f
kantikorn <kantikorn@users.noreply.github.com>
2024-05-15 01:31:33 +0000

    deploy: a60ae5f09bbf41f7cfa2d1c87ec2b7937a7f4935

--- loop.js
+++
@@ -1,8 +0,0 @@
-function loopNumbers() {
-    let result = [];
-    for(let i =1; i <= 10; i++) {
-        result.push(i)
-    }
-    return result;
-}
-module.exports = loopNumbers;
--- tests/loop.test.js
+++
@@ -1,5 +0,0 @@
-const loopNumbers = require('../loop');
-
-test('loop from 1 to 10', () =>{
-    expect(loopNumbers()).toEqual([1,2,3,4,5,6,7,8,9,10,12,13]);
-})
